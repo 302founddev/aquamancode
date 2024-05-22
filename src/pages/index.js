27be09f787d0009b0b32bc2fb249b00d000170c8
Juan Alberto R. S. <jarscode.dev@outlook.com>
2024-05-22 17:40:35 -0400

    Add icons to the project; adjust header width

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,44 @@
 import './index.css';
 import Badge from 'react-bootstrap/Badge';
+import { IconGithub } from '../icons/GitHub';
+import { IconLinkedin } from '../icons/Linkedin';
 
 
 export const Home = () => {
 
+    let linkedin = "https://linkedin.com/in/jarscodedev";
+    let github = "https://github.com/jarscodedev";
+
     return (
         <main>
             <section className='section'>
-                <h1>Hey, I am jarscode 
-                <a
-                    className='fs-6'
-                    href='https://www.linkedin.com/in/jarscodedev/' 
-                    >
-                    <Badge> Available for hire</Badge>
-                </a>
-                </h1>
-                <h2>
-                    +15 years experience. Web Developer and Content Creator. 
-                    From Madrid, Spain. I specialize in UI design, responsive web desing and visual development.
-                </h2>
-                </section>
+                <div>
+                    <h1>Hey, I am JarsCode 
+                    <a
+                        className='fs-6'
+                        href={linkedin} 
+                        >
+                        <Badge> Available for hire</Badge>
+                    </a>
+                    </h1>
+                    <p>
+                        +15 years experience.
+                        <span className='ylw-900'> Web Developer and Content Creator</span>. From Madrid, Spain.
+                        I specialize in UI design, responsive web design and visual development.
+                    </p>
+                </div>
+
+                <ul>
+                    <li>
+                        <a href={linkedin}><IconLinkedin /></a>
+                    </li>
+                    <li>
+                        <a href={github}>
+                        <IconGithub />
+                        </a>
+                    </li>
+                </ul>
+            </section>
         </main>
     )
 }
