d0ba5e08c0d56a4b1078fc98d2ded269594db0e8
Juan Alberto R. S. <jarscode.dev@outlook.com>
2024-07-22 16:03:09 +0200

    new dist done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,7 +11,7 @@ export const Work = () => {
             description: "Agencia como puerta de entrada a aventuras globales inolvidables, ofreciendo experiencias personalizadas que transforman cada viaje en una experiencia única.",
             link: "https://www.soltraveltour.vercel.app",
             github: "https://github.com/jarscodedev/soltraveltour.git",
-            image: "/public/suntraveltour.svg",
+            image: "/suntraveltour.svg",
             live: true
         },
         {
@@ -19,7 +19,7 @@ export const Work = () => {
             description: "Web diseñada para consumir una API con los personajes de Rick and Morty, de uso practico donde se muestran diferentes tipos de datos sobre los personajesl.",
             link: "https://fetchrickandmorty.vercel.app",
             github: "https://github.com/jarscodedev/fetchrickandmorty.git",
-            image: "/public/fetchrick.svg",
+            image: "/fetchrick.svg",
             live: true
         }
     ];
