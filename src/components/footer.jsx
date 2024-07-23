e0fb827db7325340b01e9906117587f1b3579d53
Juan Alberto R. S. <jarscode.dev@outlook.com>
2024-07-23 12:33:24 +0200

    footer style fixing

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,8 +5,8 @@ export const Footer = () => {
 
         <footer className="footer">
             <div>
-                <span>© 2023 <a href="https://www.jarscode.dev/" target='_blank'> jarscode</a>. Casi todos los derechos reservados.</span>
-                <span className="footer-span">
+                <span className='footer-span'>© 2023 <a href="https://www.jarscode.dev/" target='_blank'> jarscode</a>. Casi todos los derechos reservados.</span>
+                <span className='footer-span gap'>
                     <a href="/#sobremi" className="footer-link">Sobre mí</a>
                     <a href="mailto:jarscode.dev@outlook.com" className="footer-link">Contacto</a>
                 </span>
