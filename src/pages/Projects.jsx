60d88443262b6fbc3d3a7bbe3a549f71947d0e00
Juan Alberto <jarscode.dev@outlook.com>
2024-08-26 15:14:50 +0200

    fix -- web content

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,64 +5,72 @@ import { FaCode } from "react-icons/fa";
 
 export const Work = () => {
 
-    const PROJECTS = [
-        {
-            title: "Sol Traveltour - Una pre-landing page de una agencia de viajes.",
-            description: "Agencia como puerta de entrada a aventuras globales inolvidables, ofreciendo experiencias personalizadas que transforman cada viaje en una experiencia única.",
-            link: "https://soltraveltour-agency.vercel.app",
-            github: "https://github.com/jarscodedev/soltraveltour-agency.git",
-            image: "/soltraveltour-agency.webp",
-            live: true
-        },
-        {
-            title: "fetchrickandmorty - Consume una api y muestra sus datos.",
-            description: "Web diseñada para consumir una API con los personajes de Rick and Morty, de uso practico donde se muestran diferentes tipos de datos sobre los personajesl.",
-            link: "https://fetchrickandmorty.vercel.app",
-            github: "https://github.com/jarscodedev/fetchrickandmorty.git",
-            image: "/fetchrick.svg",
-            live: true
-        }
-    ];
+  const PROJECTS = [
+    {
+      title: "Sol Traveltour - Una pre-landing page de una agencia de viajes.",
+      description: "Agencia como puerta de entrada a aventuras globales inolvidables, ofreciendo experiencias personalizadas que transforman cada viaje en una experiencia única.",
+      link: "https://soltraveltour-agency.vercel.app",
+      github: "https://github.com/aquamancode/soltraveltour-agency.git",
+      image: "/soltraveltour-agency.webp",
+      live: true
+    },
+    {
+      title: "Aquamancode web portfolio - Portfolio personal construido en react.",
+      description: "Esta es mi carta de presentación para que me conozcan y conozcan el trabajo que estoy realizando.",
+      link: "https://aquamancode.vercel.app",
+      github: "https://github.com/aquamancode/aquamancode.git",
+      image: "/aquamancode.webp",
+      live: true
+    },
+    {
+      title: "fetchrickandmorty - Consume una api y muestra sus datos.",
+      description: "Web diseñada para consumir una API con los personajes de Rick and Morty, de uso practico donde se muestran diferentes tipos de datos sobre los personajesl.",
+      link: "https://fetchrickandmorty.vercel.app",
+      github: "https://github.com/aquamancode/fetchrickandmorty.git",
+      image: "/fetchrick.svg",
+      live: true
+    }
+  ];
 
-    return (
-        <section className='section'>
-            <div className='title' id='proyectos'>
-                <FaCode className='project-icon' />
-                <h3>Proyectos</h3>
-            </div>
-            {PROJECTS.map((work, index) => (
-                <article key={`work-${index}`}>
-                    <div>
-                        <img src={work.image} alt={`Captura de pantalla del proyecto ${work.title}`} />
-                    </div>
-                    <div>
-                        <h3>{work.title}</h3>
+  return (
+    <section className='section'>
+      <div className='title' id='proyectos'>
+        <FaCode className='project-icon' />
+        <h3>Proyectos</h3>
+      </div>
+      {PROJECTS.map((work, index) => (
+        <article key={`work-${index}`}>
+          <div>
+            <img src={work.image} alt={`Captura de pantalla del proyecto ${work.title}`} />
+          </div>
+          <div>
+            <h3>{work.title}</h3>
 
-                        <p>{work.description}</p>
+            <p>{work.description}</p>
 
-                        {work.live ? (
-                            <div className="links">
-                                <a className="p-links" href={work.github} target="_blank" rel="noopener noreferrer">
-                                    <Github /> Source
-                                </a>
-                                <a className="p-links" href={work.link} target="_blank" rel="noopener noreferrer">
-                                    <DeveloperStudentClub /> Preview
-                                </a>
-                            </div>
-                        ) : 
-                        <div className="links">
-                            <a className="p-links" href={work.github} target="_blank" rel="noopener noreferrer">
-                                <Github /> Source
-                            </a>
-                            <a className="p-links" href={work.link} target="_blank" rel="noopener noreferrer">
-                                <DeveloperStudentClub /> Preview
-                            </a>
-                        </div>}
+            {work.live ? (
+              <div className="links">
+                <a className="p-links" href={work.github} target="_blank" rel="noopener noreferrer">
+                  <Github /> Source
+                </a>
+                <a className="p-links" href={work.link} target="_blank" rel="noopener noreferrer">
+                  <DeveloperStudentClub /> Preview
+                </a>
+              </div>
+            ) :
+              <div className="links">
+                <a className="p-links" href={work.github} target="_blank" rel="noopener noreferrer">
+                  <Github /> Source
+                </a>
+                <a className="p-links" href={work.link} target="_blank" rel="noopener noreferrer">
+                  <DeveloperStudentClub /> Preview
+                </a>
+              </div>}
 
-                    </div>
-                </article>
-            ))}
-        </section>
-    );
+          </div>
+        </article>
+      ))}
+    </section>
+  );
 };
 
