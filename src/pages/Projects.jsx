b5039cc97785c409997a191ca56e2231e4f10467
Juan Alberto <jarscode.dev@outlook.com>
2025-01-04 19:19:35 +0100

    Update project section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,26 +7,26 @@ export const Work = () => {
 
   const PROJECTS = [
     {
-      title: "Sol Traveltour - Web de reservas para una agencia de viajes.",
-      description: "Agencia como puerta de entrada a aventuras globales inolvidables, ofreciendo experiencias personalizadas que transforman cada viaje en una experiencia única.",
-      link: "https://soltraveltour-agency.vercel.app",
-      github: "https://github.com/4quacode/soltraveltour-agency.git",
-      image: "/soltraveltour-agency.webp",
+      title: "Beeslug - Streamline Your URL Management with Bee.",
+      description: "Bee is an open-source tool designed to simplify the creation, organization, and tracking of URLs. With an intuitive interface and powerful features, Bee allows you to manage links efficiently, saving time and enhancing the analysis of your URLs' performance.",
+      link: "https://beeslug.vercel.app",
+      github: "https://github.com/302FoundDev/bee",
+      image: "/beeslug.webp",
       live: true
     },
     {
-      title: "Aquamancode web portfolio - Portfolio personal construido en react.",
-      description: "Esta es mi carta de presentación para que me conozcan y conozcan el trabajo que estoy realizando. Mejoras en camino...",
-      link: "https://aquamancode.vercel.app",
-      github: "https://github.com/4quacode/4quacode-homepage.git",
-      image: "/aquamancode.webp",
+      title: "Sol Traveltour - Web de reservas para una agencia de viajes.",
+      description: "Agencia como puerta de entrada a aventuras globales inolvidables, ofreciendo experiencias personalizadas que transforman cada viaje en una experiencia única.",
+      link: "https://soltraveltour-agency.vercel.app",
+      github: "https://github.com/302FoundDev/soltraveltour-agency",
+      image: "/soltraveltour-agency.webp",
       live: true
     },
     {
       title: "fetchrickandmorty - Consume una api y muestra sus datos.",
       description: "Web diseñada para consumir una API con los personajes de Rick and Morty, de uso practico donde se muestran diferentes tipos de datos sobre los personajesl.",
       link: "https://fetchrickandmorty.vercel.app",
-      github: "https://github.com/4quacode/fetchrickandmorty.git",
+      github: "https://github.com/302FoundDev/fetchrickandmorty",
       image: "/fetchrick.svg",
       live: true
     }
@@ -39,7 +39,9 @@ export const Work = () => {
         <h3>Proyectos</h3>
       </div>
       {PROJECTS.map((work, index) => (
-        <article key={`work-${index}`}>
+        <article
+          key={`work-${index}`}
+        >
           <div>
             <img src={work.image} alt={`Captura de pantalla del proyecto ${work.title}`} />
           </div>
